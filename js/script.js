519a68a05081d6c76a80e2b30e5680f7691fbcdb
Sandeep-508 <elsenothing321@gmail.com>
2024-04-02 09:45:07 +0530

    doing the price section

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ menuicon.addEventListener("click", () => {
 
     });
 });
+
+let accord_items = document.querySelectorAll(".accord_item");
+let disp_text = document.querySelector(".disp_text");
+let svg = document.querySelector(".svg_icon_box");
+let btn_click = document.querySelector(".click_btn");
+btn_click.classList.add("line_color");
+disp_text.style.display = "block";
+svg.classList.remove("accord_flex_prop");
+
+accord_items.forEach(item => {
+    let disp_text = item.querySelector(".disp_text");
+    let svg = item.querySelector(".svg_icon_box");
+    let btn = item.querySelector(".click_btn");
+    btn.addEventListener("click", () => {
+        accord_items.forEach(element => {
+            if (element !== item) {
+                let disp_text = element.querySelector(".disp_text");
+                let svg = element.querySelector(".svg_icon_box");
+                let btn = element.querySelector(".click_btn");
+                disp_text.style.display = "none";
+                svg.classList.add("accord_flex_prop");
+                btn.classList.remove("line_color");
+                btn.classList.add("light_white_clr");
+            }
+        });
+        let text_disp = window.getComputedStyle(disp_text).display;
+        if (text_disp === "none") {
+            disp_text.style.display = "block";
+            svg.classList.remove("accord_flex_prop");
+            btn.classList.add("line_color");
+        } else {
+            disp_text.style.display = "none";
+            svg.classList.add("accord_flex_prop");
+            btn.classList.remove("line_color");
+        }
+    });
+});
